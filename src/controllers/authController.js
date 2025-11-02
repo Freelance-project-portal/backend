1ecf87c68d27229315fe6b3bc26ed0b3dbeda0a6
Arjav Jain <arjav2768@gmail.com>
2025-11-02 23:01:50 +0530

    Minor changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,13 +8,13 @@ const generateToken = (id) => {
 // REGISTER
 export const register = async (req, res) => {
   try {
-    const { name, email, password, role, skills } = req.body;
+    const { name, email, password, role } = req.body;
 
     const userExists = await User.findOne({ email });
     if (userExists)
       return res.status(400).json({ message: "User already exists" });
 
-    const user = await User.create({ name, email, password, role, skills });
+    const user = await User.create({ name, email, password, role });
 
     res.status(201).json({
       _id: user._id,
